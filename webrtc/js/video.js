1a6710821959f4078c5647842f3eade20d33c158
Sunil D S <sunil3590@gmail.com>
2018-01-13 16:12:14 +0530

    minor changes

--- webrtc/js/video.js
+++ webrtc/js/video.js
@@ -13,7 +13,7 @@ var video = document.querySelector('video');
 
 // Put variables in global scope to make them available to the browser console.
 var constraints = window.constraints = {
-  audio: false,
+  audio: true,
   video: true
 };
 
